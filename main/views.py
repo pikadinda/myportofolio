1bc59023f43ec3fe1c435c3af197fe39e5013b0d
Adinda Pika Fauziah <dindapikafauziah@gmail.com>
2026-09-09 21:55:15 +0700

    Tutorial 02: Implementasi model-view-template (MVT) pada Djanggo

--- main/views.py
+++ main/views.py
@@ -10,7 +10,7 @@ def show_main(request):
     context = {
         "name": "Adinda Pika Fauziah",
         "npm": "2506533646",
-        "study_program": "S1 Sistem Informasi",
+        "study_program": "S1 Information Systems",
         "bio": (
             "Information Systems student at Universitas Indonesia."
             "Just a student who enjoys documenting life through photos, discovering great bakeries, and keeping things simple."
