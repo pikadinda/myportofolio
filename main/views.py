d0596f82563ecc7a9b9ad5700b7fe752007687c5
Adinda Pika Fauziah <dindapikafauziah@gmail.com>
2026-09-09 17:40:50 +0700

    Tutorial 02: Implementasi model-view-template (MVT) pada Djanggo

---
+++ main/views.py
@@ -0,0 +1,29 @@
+
+
+# Create your views here.
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Adinda Pika Fauziah",
+        "npm": "2506533646",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Information Systems student at Universitas Indonesia."
+            "Just a student who enjoys documenting life through photos, discovering great bakeries, and keeping things simple."
+           
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Adinda Pika Fauziah",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
+
