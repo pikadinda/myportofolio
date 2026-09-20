599bb438137a71d156e592935bcd24e72805ec76
Adinda Pika Fauziah <dindapikafauziah@gmail.com>
2026-09-20 19:33:15 +0700

    tugas 03 : add experience add/create form

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", views.show_main, name="show_main"),
     path("education/", views.show_education, name="show_education"),
     path("experience/", views.show_experience, name="show_experience"),
+    path("experience/add/", views.show_experience_form, name="show_experience_form"),
     path("skills/", views.show_skills, name="show_skills"),
     path("interests/", views.show_interests, name="show_interests"),
     path("skills/add/", views.show_skill_form, name="show_skill_form"),
